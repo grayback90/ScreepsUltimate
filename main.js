eb2b3b2af32540243cef48bea3a96f3f82a1fa1a
Kenny Maeder <maeder@softwareforen.de>
2019-03-18 11:04:31 +0100

    first files taken from ScreepsLegacy, no changes in these files

---
+++ main.js
@@ -0,0 +1,45 @@
+/**********************************************
+*
+* file: main.js
+* date: 18.03.2019
+* version: 0.1
+*
+* funtions: main logic
+*
+**********************************************/
+
+// import modules
+require('prototype.creep');
+require('prototype.tower');
+require('prototype.spawn');
+
+module.exports.loop = function () {
+    // check for memory entries of died creeps by iterating over Memory.creeps
+    for (let name in Memory.creeps) {
+        // and checking if the creep is still alive
+        if (Game.creeps[name] == undefined) {
+            // if not, delete the memory entry
+            delete Memory.creeps[name];
+        }
+    }
+
+    // for each creeps
+    for (let name in Game.creeps) {
+        // run creep logic
+        Game.creeps[name].runRole();
+    }
+
+    // find all towers
+    var towers = _.filter(Game.structures, s => s.structureType == STRUCTURE_TOWER);
+    // for each tower
+    for (let tower of towers) {
+        // run tower logic
+        tower.defend();
+    }
+
+    // for each spawn
+    for (let spawnName in Game.spawns) {
+        // run spawn logic
+        Game.spawns[spawnName].spawnCreepsIfNecessary();
+    }
+};
---
+++ prototype.tower.js
@@ -0,0 +1,22 @@
+/**********************************************
+*
+* file: prototype.tower.js
+* date: 18.03.2019
+* version: 0.1
+*
+* funtions: logic for all towers to find
+*           and attack enemies
+*
+**********************************************/
+
+// create a new function for StructureTower
+StructureTower.prototype.defend =
+    function () {
+        // find closes hostile creep
+        var target = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        // if one is found...
+        if (target != undefined) {
+            // ...FIRE!
+            this.attack(target);
+        }
+};
